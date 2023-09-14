3c481858af75fc6f48cb78ca4fc0c25d8d6e9127
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-09-14 18:49:01 -0300

    Implementado herança nas classes Nokia e Iphone, e feito override no metodo de instalarAplicativo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia(numero: "123456789", modelo: "Tijolao", imei: "1111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Iphone iphone = new Iphone(numero: "2468101214", modelo: "Iphone 12", imei: "2222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
